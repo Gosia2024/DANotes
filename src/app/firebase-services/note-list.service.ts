25b377f091344f46b6e212ca35d2c7c9519c6d99
Gosia <lm.sprachen@gmail.com>
2024-07-25 10:53:46 +0200

    fehler

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,9 +1,31 @@
-import { Injectable } from '@angular/core';
-
+import { Injectable, inject } from '@angular/core';
+import {Note} from '../interfaces/note.interface'
+import { collectionData, Firestore, collection, doc } from '@angular/fire/firestore';
 @Injectable({
   providedIn: 'root'
 })
 export class NoteListService {
 
-  constructor() { }
+  trashNotes: Note[] = [];
+  normaleNotes: Note[] = [];
+
+  items$;
+  firestore: Firestore = inject(Firestore);
+
+  constructor() {
+    this.items$ = collectionData(this.getNotesRef());
+  }
+
+  getNotesRef(){
+    return collection(this.firestore, 'notes');
+  }
+
+
+getTrashRef(){
+  return collection(this.firestore, 'trash');
+}
+
+getSingleDocRef(colId:string, docId:string){
+return doc(collection(this.firestore, colId), docId);
+}
 }
