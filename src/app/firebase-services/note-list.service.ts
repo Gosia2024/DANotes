4ac75972930890af6738826336449351e9b9b489
Gosia <lm.sprachen@gmail.com>
2024-07-25 15:01:50 +0200

    fehler NoteListComTs

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,6 +1,8 @@
 import { Injectable, inject } from '@angular/core';
 import {Note} from '../interfaces/note.interface'
-import { collectionData, Firestore, collection, doc } from '@angular/fire/firestore';
+import {  Firestore, collection, doc, onSnapshot } from '@angular/fire/firestore';
+import {Observable} from 'rxjs';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -10,22 +12,49 @@ export class NoteListService {
   normaleNotes: Note[] = [];
 
   items$;
+  items;
+
+unsubList;
+unsubSingle;
+
   firestore: Firestore = inject(Firestore);
 
   constructor() {
-    this.items$ = collectionData(this.getNotesRef());
-  }
 
-  getNotesRef(){
-    return collection(this.firestore, 'notes');
+    this.unsubList = onSnapshot(this.getNotesRef(), (list) => {
+      list.forEach(element => {
+        console.log(element);
+      });
+    });
+
+ this.unsubSingle = onSnapshot(this.getSingleDocRef("notes", "a66sdfdfffff"), (element) => {
+
+ });
+
+ this.unsubSingle();
+ this.unsubList();
+
+
+
+ this.items$ = collectionData(this.getNotesRef());
+ this.items = this.items$.subscribe( (list) => {
+  list.forEach(element => {
+    console.log(element);
+  });
+ })
+ this.items.unsubscribe();
+}
+
+ getNotesRef(){
+   return collection(this.firestore, 'notes');
   }
 
 
 getTrashRef(){
-  return collection(this.firestore, 'trash');
+ return collection(this.firestore, 'trash');
 }
 
 getSingleDocRef(colId:string, docId:string){
 return doc(collection(this.firestore, colId), docId);
 }
-}
+  }
